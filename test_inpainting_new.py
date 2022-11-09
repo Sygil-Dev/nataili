f634f147afe86a09a6d2db6b4117ba9cf11fe8d4
blueturtleai <113246030+blueturtleai@users.noreply.github.com>
2022-11-09 21:08:50 +0100

    Update test_inpainting_new.py

--- test_inpainting_new.py
+++ test_inpainting_new.py
@@ -2,11 +2,12 @@ from PIL import Image
 
 from nataili.inference.inpainting import run
 
-original = Image.open("./inpaint_original.png")
-mask = Image.open("./inpaint_mask.png")
-
 config = "configs/stable-diffusion/v1-inpainting-inference.yaml"
 ckpt = "models/sd-v1-5-inpainting.ckpt"
+original = Image.open("./inpaint_original.png")
+mask = Image.open("./inpaint_mask.png")
+prompt = "a robot sitting on a bench"
+
 images = run(config, ckpt, original, mask, prompt)
-  
+
 images[0].save("robot_sitting_on_a_bench.png", format="PNG")
