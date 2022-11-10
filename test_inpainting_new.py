db35b7e459a597f13bf0cc60b41270e599b81bdd
blueturtleai <113246030+blueturtleai@users.noreply.github.com>
2022-11-10 11:13:33 +0100

    Update test_inpainting_new.py

--- test_inpainting_new.py
+++ test_inpainting_new.py
@@ -1,6 +1,5 @@
 from PIL import Image
-
-from nataili.inference.inpainting import run
+from nataili.inference.inpainting import inpainting
 
 config = "configs/stable-diffusion/v1-inpainting-inference.yaml"
 ckpt = "models/sd-v1-5-inpainting.ckpt"
@@ -8,6 +7,8 @@ original = Image.open("./inpaint_original.png")
 mask = Image.open("./inpaint_mask.png")
 prompt = "a robot sitting on a bench"
 
-images = run(config, ckpt, original, mask, prompt)
-
-images[0].save("robot_sitting_on_a_bench.png", format="PNG")
+generator = inpainting("cuda", "output_dir")
+generator.initialize_model(config, ckpt)
+generator.generate(prompt, original, mask)
+image = generator.images[0]["image"]
+image.save("robot_sitting_on_a_bench.png", format="PNG")
