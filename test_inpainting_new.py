18bc99073dfc2d7aa6e1d74076fe78fd63c0b05b
blueturtleai <113246030+blueturtleai@users.noreply.github.com>
2022-11-10 14:12:52 +0100

    Update test_inpainting_new.py

--- test_inpainting_new.py
+++ test_inpainting_new.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from nataili.inference.inpainting import inpainting
+from nataili import disable_xformers
 
 config = "configs/stable-diffusion/v1-inpainting-inference.yaml"
 ckpt = "models/sd-v1-5-inpainting.ckpt"
@@ -7,6 +8,7 @@ original = Image.open("./inpaint_original.png")
 mask = Image.open("./inpaint_mask.png")
 prompt = "a robot sitting on a bench"
 
+disable_xformers.toggle(True)
 generator = inpainting("cuda", "output_dir")
 generator.initialize_model(config, ckpt)
 generator.generate(prompt, original, mask)
