4da1f125394af44a00c96886a98961781a4e8cc4
Divided by Zer0 <mail@dbzer0.com>
2022-12-08 13:35:47 +0100

    fix

--- bridge.py
+++ bridge.py
@@ -37,7 +37,7 @@ def bridge(this_model_manager, this_bridge_data):
                             reload_data(this_bridge_data)
                             executor._max_workers = this_bridge_data.max_threads
                             logger.stats(f"Stats this session:\n{bridge_stats.get_pretty_stats()}")
-                            if len(this_bridge_data.model_names) >= this_bridge_data.number_of_dynamic_models:
+                            if len(this_bridge_data.predefined_models) >= this_bridge_data.number_of_dynamic_models:
                                 logger.warning(
                                     "You have more than half of your dynamic models predefined in models_to_load! "
                                     "This means there will be very little space to load models dynamically. "
