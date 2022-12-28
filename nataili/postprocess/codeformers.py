e94a12c13d8e3f59c774c6bc33c09b02191ca57f
ResidentChief <35794623+ResidentChief@users.noreply.github.com>
2022-12-28 14:25:46 +1300

    Update codeformers.py

--- nataili/postprocess/codeformers.py
+++ nataili/postprocess/codeformers.py
@@ -1,3 +1,4 @@
+from codeformer import CodeFormer
 from nataili.postprocessor import *
 
 
@@ -6,6 +7,10 @@ class CodeFormers(PostProcessor):
         self.filename_append = "CodeFormers"
 
     def process(self, img: PIL.Image, img_array, **kwargs) -> PIL.Image:
+        model = CodeFormer(
+            weights=None,
+            upscale=1,
+        ).to(self.device)
         output = self.model(img)
         output_array = np.array(output)
         output_image = PIL.Image.fromarray(output_array)
